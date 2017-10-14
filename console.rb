a30a9ffb8a11351aa792a85ddd57e29af1b6ea64
Ayaka Ikezawa <ikeay@Ayaka-no-MacBook-Pro.local>
2017-10-14 14:51:24 +0900

    コンソールから動かせるように

--- console.rb
+++ console.rb
@@ -3,8 +3,11 @@
 # デバッグ用途
 require './class/analyze_text'
 
+uri = URI.parse('localhost:4567')
+$redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+
 while true
   print "> "
   text = STDIN.gets.chomp
-  puts AnalyzeText.new(text).result
+  puts AnalyzeText.new(text, 'xxxxx').result
 end
